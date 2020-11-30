0c9fa870c842c6603ec7c21d322df5c4cc0b12a0
Andrei Vazovikov <mrsmurfn1@gmail.com>
2020-11-30 19:36:04 +0300

    adding new class HelloWorld

---
+++ src/main/java/com/andersen_trainee/git_test_ex/entity/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.andersen_trainee.git_test_ex.entity;
+
+public class HelloWorld {
+    private String word;
+
+    public HelloWorld(){
+        word = "Hello World !!!";
+    }
+
+    public void helloWorld(){
+        System.out.println(word);
+    }
+}
